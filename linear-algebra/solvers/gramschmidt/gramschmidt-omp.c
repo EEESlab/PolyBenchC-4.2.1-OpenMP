7593b6d9895d9b87ce035a5b8892eb4f6e4336e5
Luca Parigi <luca.parigi2@gmail.com>
2026-03-18 12:38:14 +0100

    fixed gramschmidt, added others omp

--- linear-algebra/solvers/gramschmidt/gramschmidt-omp.c
+++ linear-algebra/solvers/gramschmidt/gramschmidt-omp.c
@@ -89,8 +89,6 @@ void kernel_gramschmidt(int m, int n,
   for (k = 0; k < _PB_N; k++)
     {
       nrm = SCALAR_VAL(0.0);
-
-      #pragma omp parallel for reduction(+:nrm)
       for (i = 0; i < _PB_M; i++)
         nrm += A[i][k] * A[i][k];
       R[k][k] = SQRT_FUN(nrm);
